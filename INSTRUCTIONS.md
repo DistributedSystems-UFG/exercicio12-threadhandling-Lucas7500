0c67a46fc1f10ca43b126c43d7da88cfc54b0d54
Lucas Iglesias <lucasiglesias2704@gmail.com>
2026-05-27 20:14:07 -0300

    feat: initial commit

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,2 @@
+Com base no exemplo fornecido (repo template da tarefa), acrescente uma nova thread para executar alguma tarefa CPU-intensive, mas que admite ser interrompida caso o tempo de execução exceda um limite.
+
--- SimpleThreads.java
+++ SimpleThreads.java
@@ -6,6 +6,28 @@ public class SimpleThreads {
         System.out.format("%s: %s%n", threadName, message);
     }
 
+    static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    private static class FibonacciRunnable 
+    implements Runnable {
+    @Override
+    public void run() {
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            long result = fibonacci(i);
+            threadMessage("Fibonacci of " + i + " is " + result);
+            i++;
+        }
+        threadMessage("I wasn't done!");
+    }
+}
+
+
     private static class MessageLoop
         implements Runnable {
         public void run() {
@@ -47,23 +69,27 @@ public class SimpleThreads {
         threadMessage("Starting MessageLoop thread");
         long startTime = System.currentTimeMillis();
         Thread t = new Thread(new MessageLoop());
+        Thread t2 = new Thread(new FibonacciRunnable());
 
 	// Put the MessageLoop thread to run
         t.start();
+        t2.start();
 
         threadMessage("Waiting for MessageLoop thread to finish");
 	
         // loop until MessageLoop thread exits
-        while (t.isAlive()) {
+        while (t.isAlive() || t2.isAlive()) {
             threadMessage("Still waiting...");
             // Wait maximum of 1 second for MessageLoop thread to finish
             t.join(1000);
-            if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
+            if (((System.currentTimeMillis() - startTime) > patience) && (t.isAlive() || t2.isAlive())) {
                 threadMessage("Tired of waiting!");
 		// Force the interruption of the MainLoop thread
                 t.interrupt();
+                t2.interrupt();
                 // ...and wait for it to finish -- shouldn't be long now 
                 t.join();
+                t2.join();
             }
         }
         threadMessage("Finally!");
